e5ed28430bf711f26c9b9d42f6697655e27ab18c
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-08-27 11:20:11 -0400

    proof of concept compiles

---
+++ .CMake/toolchain_pico2w.cmake
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: MIT
+
+# How to use:
+# sudo apt install gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+# cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=../.CMake/toolchain_pico2w.cmake -DOQS_USE_OPENSSL=OFF -DOQS_PERMIT_UNSUPPORTED_ARCHITECTURE=ON -DOQS_EMBEDDED_BUILD=ON -DOQS_BUILD_ONLY_LIB=ON ..
+
+set(CMAKE_SYSTEM_NAME PICO)
+set(CMAKE_SYSTEM_PROCESSOR arm32v7)
+set(CMAKE_CROSSCOMPILING ON)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc --specs=nosys.specs)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Unconditionally set for this platform
+add_definitions( -DOQS_USE_RASPBERRY_PI )
